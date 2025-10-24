0fd347d9af8278a1db21b7af0047e4ae38da0526
jamato01 <jarrett.m.torres@gmail.com>
2025-10-23 21:07:39 -0400

    Create #initialize and #hash

---
+++ lib/hash_map.rb
@@ -0,0 +1,17 @@
+# This class creates and manipulates HashMaps
+class HashMaps
+
+  def initialize
+    @load_factor = 0.75
+    @capacity = 16
+  end
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
+end
