83c5f017609e9faf9679fe24b973b0b1cb691bf6
jamato01 <jarrett.m.torres@gmail.com>
2025-10-24 14:21:37 -0400

    Add node class
    From ruby-linked-list project

---
+++ lib/node.rb
@@ -0,0 +1,14 @@
+#This class creates and manages nodes for the linked list
+class Node
+  attr_accessor :value, :next_node
+  
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def to_s
+    "( #{@value} )"
+  end
+
+end
